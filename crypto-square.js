3c8850c7ec5b918bb65a6a4ed5d4d5809b8267f2
Chris Tignor <Chris Tignor>
2016-04-27 10:35:29 -0400

    First commit

--- crypto-square.js
+++ crypto-square.js
@@ -1,12 +1,56 @@
-// create our Crypto class
-// it takes a string as an argument
 var Crypto = function(text){
-	// store the string for use elsewhere
+
 	this.text = text;
 };
+
 Crypto.prototype.normalizePlaintext = function() {
-	return this.text;
+	
+	return this.text.replace(/[^\w]/g,"").toLowerCase();
+
 };
 
-module.exports = Crypto
+Crypto.prototype.size = function(){
+	var str = this.normalizePlaintext();
+	var length = str.length;
+	var size = Math.sqrt(length);
+	return Math.ceil(size);
+};
 
+Crypto.prototype.plaintextSegments = function() {
+	
+	var count = this.size();
+	var size = count.size;
+	var arrayLength = this.text.length;
+	var stringLength = Math.floor(arrayLength / count);
+	var str = this.normalizePlaintext();
+	var loop = stringLength;
+	var res = "";
+	for (var i = 0; i <= arrayLength; i++) 
+	{
+		res += str.substring(i,loop) + ",";
+		loop +=stringLength;
+		i += stringLength - 1;
+
+
+
+	}
+	var output = res.split(",", stringLength-1);
+	return output;
+
+};
+Crypto.prototype.ciphertext = function(){
+
+	var string = this.plaintextSegments();
+	for (var i = 0; i < this.plaintextSegments.stringLength; i++)
+	{
+		for(var j = 0; j < stringLength-1; j++)
+		{
+			var output = output + string[j].substring(i,i+1);
+		}
+		
+	}
+	return output;
+
+
+}
+module.exports = Crypto;
